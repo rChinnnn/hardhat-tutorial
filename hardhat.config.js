af3e77aa1e0f4dae3d82d9d689e256a98087d317
rChinnnn <b10121035@yuntech.edu.tw>
2023-12-14 00:03:56 +0800

    add Contracts from manifold.xyz

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.local' });
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    },
+    local: {
+      url: `http://127.0.0.1:8545/`,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
